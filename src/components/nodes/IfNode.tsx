bf9fa5744c0c9ff56a9e4190230a27efcef29cc8
ShiratsuYudachi <shiratsuyudachi@gmail.com>
2025-10-15 01:41:15 +0800

    fix: incorrect input handle for ifExpression

--- src/components/nodes/IfNode.tsx
+++ src/components/nodes/IfNode.tsx
@@ -12,47 +12,44 @@ export function IfNode() {
 				🔀 If Expression
 			</div>
 			
-			{/* Input handles */}
-			<div className="space-y-2 mb-3">
-				<div className="flex items-center relative">
-					<Handle
-						type="target"
-						position={Position.Left}
-						id="condition"
-						style={{ top: '40px' }}
-						className="w-3 h-3 bg-red-500 -left-1.5"
-					/>
-					<div className="ml-3 text-xs text-red-600">
-						condition
-					</div>
-				</div>
-				
-				<div className="flex items-center relative">
-					<Handle
-						type="target"
-						position={Position.Left}
-						id="then"
-						style={{ top: '64px' }}
-						className="w-3 h-3 bg-green-500 -left-1.5"
-					/>
-					<div className="ml-3 text-xs text-green-600">
-						then
-					</div>
-				</div>
-				
-				<div className="flex items-center relative">
-					<Handle
-						type="target"
-						position={Position.Left}
-						id="else"
-						style={{ top: '88px' }}
-						className="w-3 h-3 bg-blue-500 -left-1.5"
-					/>
-					<div className="ml-3 text-xs text-blue-600">
-						else
-					</div>
+		{/* Input handles */}
+		<div className="space-y-2 mb-3">
+			<div className="flex items-center relative">
+				<Handle
+					type="target"
+					position={Position.Left}
+					id="condition"
+					className="!absolute !left-0 !top-1/2 !-translate-y-1/2 !-translate-x-1/2 w-3 h-3 bg-red-500"
+				/>
+				<div className="ml-3 text-xs text-red-600">
+					condition
 				</div>
 			</div>
+			
+			<div className="flex items-center relative">
+				<Handle
+					type="target"
+					position={Position.Left}
+					id="then"
+					className="!absolute !left-0 !top-1/2 !-translate-y-1/2 !-translate-x-1/2 w-3 h-3 bg-green-500"
+				/>
+				<div className="ml-3 text-xs text-green-600">
+					then
+				</div>
+			</div>
+			
+			<div className="flex items-center relative">
+				<Handle
+					type="target"
+					position={Position.Left}
+					id="else"
+					className="!absolute !left-0 !top-1/2 !-translate-y-1/2 !-translate-x-1/2 w-3 h-3 bg-blue-500"
+				/>
+				<div className="ml-3 text-xs text-blue-600">
+					else
+				</div>
+			</div>
+		</div>
 
 			{/* Output handle */}
 			<Handle
